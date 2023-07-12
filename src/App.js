f66aed52ecfd44ee53830f49099c8248a980ac03
AjayVegiraju <84871992+AjayVegiraju@users.noreply.github.com>
2023-07-12 11:36:38 -0700

    directory changes

--- src/App.js
+++ src/App.js
@@ -1,150 +1,72 @@
-// // src/App.js
-// import React from 'react';
-// import { useSpotifyAPI } from './SpotifyAPI.js';
-// import './App.css'; // Import a new CSS file
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Button} from 'react-bootstrap';
-
-// function App() {
-
-//   const clientId = "9b98dc2244aa4e1988bebd9ba39d2a97";
-//   const { accessToken, profile , playlist , artists, song } = useSpotifyAPI(clientId);
-
-//   const handleSongButton = (song) => {
-//     window.open(song.preview_url, '_blank');
-//   }
-  
-//   console.log(song);
-//   console.log(playlist);
-  
-//   return (
-//     <div className="App">
-//       {/* Display the profile information if it's available */}
-//       {profile && (
-//         <div className="card">
-//           <h1>Hi {profile.display_name}!</h1>
-//           {/* Check if profile.images exists before trying to access its properties */}
-         
-//         </div>
-//       )}
-
-      
-
-//       {/* Display the artists if they're available */}
-//       {artists && (
-//         <div className="card">
-//           <h2>Your Top Artists</h2>
-//           <ul>
-//             {artists.map((artists) => (
-//               <li key={artists.id}>
-//                 <img src={artists.images?.[0]?.url} alt={artists.name} style={{ width: '100px' ,height: '100px', objectFit: 'cover'}}/>
-//                 {artists.name}</li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-
-//       {/* Display the top song if they're available */}
-//       {song && (
-//         <div className="card">
-//           <h2>Your Top Tracks</h2>
-//           <ul>
-//             {song.map((track) => (
-//               <li key={track.id}>
-//                 <img src={track.album.images?.[0]?.url} alt={track.name} style={{ width: '100px',height: '100px', objectFit: 'cover'}}/>
-//                 <li><Button variant = "primary" onClick = {() => handleSongButton(track)}>{track.name}</Button></li>
-//                 </li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-
-//       {/* Display the playlist if they're available */}
-//       {playlist && (
-//         <div className="card">
-//           <h2>Your Top Playlists: </h2>
-//           <ul>
-//             {playlist.map((playlist) => (
-//                <li key={playlist.id}>
-//                  <img src={playlist.images?.[0]?.url} alt={playlist.name} style={{ width: '100px' ,height: '100px', objectFit: 'cover'}}/>
-//                  {playlist.name}
-//                </li>
-//                ))}
-//           </ul>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useSpotifyAPI } from './SpotifyAPI.js';
+import { useSpotifyAPI } from './utils/data.js';
+import './utils/SpotifyAuth.js';
 import Login from './Login.js';
 import { Card, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 
 
 
 
+
 function App() {
   const clientId = "9b98dc2244aa4e1988bebd9ba39d2a97";
-  const { accessToken, profile , playlist , artists, song } = useSpotifyAPI(clientId);
+  const { accessToken, profile, playlist, artists, song } = useSpotifyAPI(clientId);
 
   const handleSongButton = (song) => {
     window.open(song.preview_url, '_blank');
-  }
-  
+  };
+
   return (
-    <div className="App">
+    <div className="container mx-auto">
       {profile && (
-        <Card style={{ width: '18rem' }}>
-          <Card.Header>Hi {profile.display_name}!</Card.Header>
-        </Card>
+        <div className="card">
+          <div className="card-header">Hi {profile.display_name}!</div>
+        </div>
       )}
 
       {artists && (
-        <Card style={{ width: '18rem' }}>
-          <Card.Header>Your Top Artists</Card.Header>
-          <ListGroup variant="flush">
-            {artists.map((artists) => (
-              <ListGroupItem key={artists.id}>
-                <img src={artists.images?.[0]?.url} alt={artists.name} style={{ width: '100px' ,height: '100px', objectFit: 'cover'}}/>
-                {artists.name}
-              </ListGroupItem>
+        <div className="card">
+          <div className="card-header">Your Top Artists</div>
+          <ul className="list-group list-group-flush">
+            {artists.map((artist) => (
+              <li key={artist.id} className="list-group-item flex items-center">
+                <img src={artist.images?.[0]?.url} alt={artist.name} className="w-16 h-16 rounded-full mr-4" />
+                <span>{artist.name}</span>
+              </li>
             ))}
-          </ListGroup>
-        </Card>
+          </ul>
+        </div>
       )}
 
       {song && (
-        <Card style={{ width: '18rem' }}>
-          <Card.Header>Your Top Tracks</Card.Header>
-          <ListGroup variant="flush">
+        <div className="card">
+          <div className="card-header">Your Top Tracks</div>
+          <ul className="list-group list-group-flush">
             {song.map((track) => (
-              <ListGroupItem key={track.id}>
-                <img src={track.album.images?.[0]?.url} alt={track.name} style={{ width: '100px' ,height: '100px', objectFit: 'cover'}}/>
-                <Button variant="primary" onClick={() => handleSongButton(track)}>{track.name}</Button>
-              </ListGroupItem>
+              <li key={track.id} className="list-group-item flex items-center">
+                <img src={track.album.images?.[0]?.url} alt={track.name} className="w-16 h-16 rounded-full mr-4" />
+                <button className="btn btn-primary" onClick={() => handleSongButton(track)}>
+                  {track.name}
+                </button>
+              </li>
             ))}
-          </ListGroup>
-        </Card>
+          </ul>
+        </div>
       )}
 
       {playlist && (
-        <Card style={{ width: '18rem' }}>
-          <Card.Header>Your Top Playlists:</Card.Header>
-          <ListGroup variant="flush">
+        <div className="card">
+          <div className="card-header">Your Top Playlists</div>
+          <ul className="list-group list-group-flush">
             {playlist.map((playlist) => (
-              <ListGroupItem key={playlist.id}>
-                <img src={playlist.images?.[0]?.url} alt={playlist.name} style={{ width: '100px' ,height: '100px', objectFit: 'cover'}}/>
-                {playlist.name}
-              </ListGroupItem>
+              <li key={playlist.id} className="list-group-item flex items-center">
+                <img src={playlist.images?.[0]?.url} alt={playlist.name} className="w-16 h-16 rounded-full mr-4" />
+                <span>{playlist.name}</span>
+              </li>
             ))}
-          </ListGroup>
-        </Card>
+          </ul>
+        </div>
       )}
     </div>
   );
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,5 +1,6 @@
-// src/App.js
+import { getAccessToken, redirectToAuthCodeFlow } from "./SpotifyAuth";
 import { useState, useEffect } from 'react';
+
 
 export const useSpotifyAPI = (clientId) => {
     const [accessToken, setAccessToken] = useState(null);
@@ -47,66 +48,7 @@ export const useSpotifyAPI = (clientId) => {
     return { accessToken, playlist, profile, artists, song };
 }
 
-export async function redirectToAuthCodeFlow(clientId) {
-    const verifier = generateCodeVerifier(128);
-    const challenge = await generateCodeChallenge(verifier);
 
-    localStorage.setItem("verifier", verifier);
-
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("scope", "user-read-private user-read-email user-top-read playlist-read-private");
-    params.append("code_challenge_method", "S256");
-    params.append("code_challenge", challenge);
-
-    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
-}
-
-function generateCodeVerifier(length) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
-
-async function generateCodeChallenge(codeVerifier) {
-    const data = new TextEncoder().encode(codeVerifier);
-    const digest = await window.crypto.subtle.digest('SHA-256', data);
-    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
-}
-
-export async function getAccessToken(clientId, code) {
-    const verifier = localStorage.getItem("verifier");
-
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("grant_type", "authorization_code");
-    params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("code_verifier", verifier);
-
-    const response = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: params
-    });
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    const { access_token } = data;
-    return access_token;
-}
 async function fetchProfile(token) {
     try {
         const response = await fetch("https://api.spotify.com/v1/me", {
@@ -206,15 +148,6 @@ async function fetchSong(token) {
 }
 
 
-
-// async function getRecommendations(){
-//     // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-recommendations
-//     return (await fetchWebApi(
-//       `v1/recommendations?limit=5&seed_tracks=${topTracksIds.join(',')}`, 'GET'
-//     )).tracks;
-// }
-
-
 // Update the populateUI function to fetch and display the top playlists and favorite artists
 // async function populateUI(profile, token) {
 //     // Existing code for populating profile information
@@ -262,5 +195,3 @@ async function fetchSong(token) {
 //         favoriteTracksElement.appendChild(songItem);
 //     });
 // }
-
-
