9f34e47cef71c5032a632f570a90afe361dc0c57
AjayVegiraju <84871992+AjayVegiraju@users.noreply.github.com>
2023-07-12 16:38:20 -0700

    song card margins

--- src/App.js
+++ src/App.js
@@ -12,6 +12,14 @@ function App() {
   const { accessToken, profile, playlist, artists, song } = useSpotifyAPI(clientId);
   const [scrollPosition, setScrollPosition] = useState(0);
 
+  const [loading, setLoading] = useState(true);
+
+  const handleSongButtonClick = (song) => {
+    // Perform desired action when the song button is clicked
+    // Example: Open the song's Spotify URL in a new window
+    window.open(song.external_urls.spotify, '_blank');
+  };
+
   useEffect(() => {
     const handleScroll = () => {
       const currentPosition = window.pageYOffset;
@@ -20,10 +28,15 @@ function App() {
 
     window.addEventListener('scroll', handleScroll);
 
+    // Update loading state when profile data has been fetched
+    if (profile) {
+      setLoading(false);
+    }
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [profile]); // Add profile as a dependency
 
   const calculateFadeOut = () => {
     const maxScroll = window.innerHeight; // Set maxScroll to window height
@@ -32,28 +45,40 @@ function App() {
     return { opacity, transform };
   };
 
+  const calculateZoomIn = () => {
+    const maxScroll = window.innerHeight; 
+    const scale = scrollPosition < maxScroll ? 0.5 + (scrollPosition / maxScroll * 0.5) : 1; // Starts at 0.5 scale and increases to 1
+    const transform = `scale(${scale})`;
+    return { transform };
+  };
+
+  // Display loading state
+  if (loading) {
+    return <div>Loading...</div>; // Or replace with your own loading indicator
+  }
+
   return (
     <div className="bg-black min-h-screen">
-      <header className="h-screen flex items-center justify-center text-white text-6xl fixed top-0 left-0 right-0 z-10">
-      <h1 className="hero" style={calculateFadeOut()}>{profile ? `Hi ${profile.display_name}!` : ''}</h1>
+      <header className="h-screen flex items-center justify-center text-white text-3xl fixed top-0 left-0 right-0 z-10">
+        <h1 className="hero" style={calculateFadeOut()}>Hi {profile.display_name}!</h1>
       </header>
-    
-      <div className="grid grid-cols-4 gap-4">
 
-      <h1 style={calculateFadeOut()}> Your Top Songs </h1>
+      <div className="grid grid-cols-4 gap-8">
+
+      <h1 style={calculateZoomIn()}> Your Top Songs </h1>
   {song &&
     song.map((track) => (
       <div key={track.id} className="bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        <img
+        <button className="song-button" onClick={() => handleSongButtonClick(track)}><img
           className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
           src={track.album.images?.[0]?.url}
           alt={track.name}
           width="384"
           height="512"
-        />
+        /></button>
         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
           <blockquote>
-            <p className="text-lg font-medium">
+            <p className="text-lg md:text-left font-medium">
               {track.name}
             </p>
           </blockquote>
@@ -72,7 +97,7 @@ function App() {
 </div>
 
       <div className="grid grid-cols-4 gap-4">
-        <h1>Top Artists</h1>
+      <h1 style={calculateZoomIn()}> Your Top Artists </h1>
         
         {artists &&
           artists.map((artist) => (
@@ -96,23 +121,31 @@ function App() {
           ))}
       </div>
 
-    
 
-      <section className="py-20">
-        <div className="container mx-auto">
-          <h2 className="text-white text-4xl mb-8">Top Playlists</h2>
-
-          <div className="grid grid-cols-4 gap-4">
-            {playlist &&
-              playlist.map((playlist) => (
-                <div key={playlist.id} className="bg-white p-4 text-center shadow-lg rounded-lg">
-                  <img src={playlist.images?.[0]?.url} alt={playlist.name} className="w-40 h-40 mx-auto mb-4 rounded" />
-                  <p className="text-lg font-bold">{playlist.name}</p>
-                </div>
-              ))}
-          </div>
-        </div>
-      </section>
+      <div className="grid grid-cols-4 gap-4">
+      <h1 style={calculateZoomIn()}> Your Top Playlists </h1>
+        
+        {playlist&&
+          playlist.map((playlist) => (
+            <div key={playlist.id} className="bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+              <img
+                className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+                src={playlist.images?.[0]?.url}
+                alt={playlist.name}
+                width="384"
+                height="512"
+              />
+              <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+                <blockquote>
+                  <p className="text-lg font-medium">
+                    {playlist.name}
+                  </p>
+                </blockquote>
+              
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
